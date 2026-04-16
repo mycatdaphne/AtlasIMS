3142d13b20c0219c120874b9e5b1afe697ab2b0c
Samuel Crist <mycatdaphne@gmail.com>
2026-04-16 16:08:42 -0700

    add firestore init in main

--- atlas_ims/lib/main.dart
+++ atlas_ims/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:atlas_ims/views/sett_sub_p/sett_gen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'data/firebase_options.dart';
 import 'views/home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AtlasIMS());
 }
 
